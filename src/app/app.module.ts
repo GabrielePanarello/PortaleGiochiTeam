bc6ad4c2086d5fd081df16b0bdbb5187f3dc058e
SimoneBergomi <bergomi.simone@gmail.com>
2018-03-23 09:49:41 +0100

    desing edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -21,6 +22,7 @@ import { MenuService } from './services/menu.service';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     MainRouter
   ],
   providers: [ListaGameService, MenuService],
